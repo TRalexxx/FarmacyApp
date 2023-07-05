a24d93fbed7c8b42f004059cf1d7986f4ea828b8
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2023-07-05 14:57:24 +0300

    made a MainVM class

--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using PharmacyApp.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PharmacyApp.ViewModel
 {
-    class MainVM
+    class MainVM : INotifyPropertyChanged
     {
+        public ObservableCollection<Drug> Drugs { get; set; }
 
+        private Drug selectedDrug;
+
+        public Drug SelectedDrug
+        {
+            get { return selectedDrug; }
+            set { selectedDrug = value; OnPropertyChanged("SelectedDrug"); }
+        }
+
+        private RelayCommand myVar;
+
+        public RelayCommand MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

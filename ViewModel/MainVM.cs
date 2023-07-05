ad982ee7a04cafaf0a8030e60cc7a35dfff128c0
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2023-07-05 15:09:16 +0300

    write simple mainwindow interface

--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using AppContext = PharmacyApp.Model.AppContext;
 
 namespace PharmacyApp.ViewModel
 {
@@ -23,15 +24,127 @@ namespace PharmacyApp.ViewModel
             set { selectedDrug = value; OnPropertyChanged("SelectedDrug"); }
         }
 
-        private RelayCommand myVar;
+        private RelayCommand addCommand;
 
-        public RelayCommand MyProperty
+        public RelayCommand AddCommand
         {
-            get { return myVar; }
-            set { myVar = value; }
+            get
+            {
+                if (addCommand == null)
+                    addCommand = new RelayCommand(AddDrug);
+
+                return addCommand;
+            }
         }
 
+        private RelayCommand deleteCommand;
 
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                if (deleteCommand == null)
+                    deleteCommand = new RelayCommand(DeleteDrug);
+
+                return deleteCommand;
+            }
+
+        }
+
+        private RelayCommand updateCommand;
+
+        public RelayCommand UpdateCommand
+        {
+            get
+            {
+                if (updateCommand == null)
+                    updateCommand = new RelayCommand(UpdateDrug);
+
+                return updateCommand;
+            }
+        }
+
+        public MainVM()
+        {
+            using (AppContext context = new AppContext())
+            {
+                Drugs = new ObservableCollection<Drug>();
+
+                List<Drug> baseUsers = context.Drugs.ToList();
+
+                if (baseUsers != null)
+                {
+                    foreach (var item in baseUsers)
+                    {
+                        Drugs.Add(item);
+                    }
+                }
+            }
+            if (Drugs.Count > 0) SelectedDrug = Drugs[0];
+        }
+
+        public void AddDrug()
+        {
+            using (AppContext context = new AppContext())
+            {
+                context.Drugs.Add(new Drug() { Name = "enter name", Description = "enter descriprion", Photo = "ented photo url" });
+                context.SaveChanges();
+
+                Drugs.Clear();
+
+                List<Drug> baseUsers = context.Drugs.ToList();
+
+                if (baseUsers != null)
+                {
+                    foreach (var item in baseUsers)
+                    {
+                        Drugs.Add(item);
+                    }
+                }
+            }            
+        }
+
+        public void UpdateDrug()
+        {
+            using (AppContext context = new AppContext())
+            {
+                context.Drugs.Update(SelectedDrug);
+                context.SaveChanges();
+
+                Drugs.Clear();
+
+                List<Drug> baseUsers = context.Drugs.ToList();
+
+                if (baseUsers != null)
+                {
+                    foreach (var item in baseUsers)
+                    {
+                        Drugs.Add(item);
+                    }
+                }
+            }
+        }
+
+        public void DeleteDrug()
+        {
+            using (AppContext context = new AppContext())
+            {
+                context.Drugs.Remove(SelectedDrug);
+                context.SaveChanges();
+
+                Drugs.Clear();
+
+                List<Drug> baseUsers = context.Drugs.ToList();
+
+                if (baseUsers != null)
+                {
+                    foreach (var item in baseUsers)
+                    {
+                        Drugs.Add(item);
+                    }
+                }
+            }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
